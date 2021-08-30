f1c9d0169bbd7d90b47d6ad2a30007c3c57a7693
taile47711 <taile47711@gmail.com>
2021-08-30 15:17:18 +0700

    update1

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,15 +2,10 @@ package tests;
 
 import common.Constant;
 import helpers.DriverHelper;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class BaseTest {
-
-
     @BeforeMethod
     public void setup() {
         DriverHelper.initDriver();
